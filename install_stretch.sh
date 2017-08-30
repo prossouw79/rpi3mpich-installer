e40c784c5c6eab1a23f04da8ed2d23fa1d65f3c0
prossouw79 <prossouw79@gmail.com>
2017-08-30 19:11:24 +0200

    added escape for failed download

--- install_stretch.sh
+++ install_stretch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # ******************************
 # Based on: https://gist.githubusercontent.com/JordiCorbilla/2b3dde3a497278ee2e5aafb05471d2e5/raw/3788c235e0e5e36569517b2edcec4bd15f2f762e/mpich3install.sh
 # These steps will allow you to install MPICH3 to your Raspbian Stretch distro
@@ -9,24 +11,29 @@ sudo apt dist-upgrade -y
 sudo apt install build-essential gfortran -y
 
 sudo mkdir /home/pi/mpich3
+sudo wget -nc -O /home/pi/mpich3/mpich-3.2.tar.gz  http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
 cd /home/pi/mpich3
 
-sudo wget -nc http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
+FILE="mpich-3.2.tar.gz"
+if [ -f $FILE ]; then
+    echo "$FILE exists. Continuing..."
 
-sudo tar xfz mpich-3.2.tar.gz
+    sudo tar xfz mpich-3.2.tar.gz
 
-sudo mkdir /home/rpimpi/
-sudo mkdir /home/rpimpi/mpi-install
-mkdir /home/pi/mpi-build
+    sudo mkdir /home/rpimpi/
+    sudo mkdir /home/rpimpi/mpi-install
+    mkdir /home/pi/mpi-build
 
-cd /home/pi/mpich3/mpich-3.2/
-sudo ./configure -prefix=/home/rpimpi/mpi-install
-sudo make -j4
-sudo make install
+    cd /home/pi/mpich3/mpich-3.2/
+    sudo ./configure -prefix=/home/rpimpi/mpi-install
+    sudo make -j4
+    sudo make install
 
-cd ..
-echo "PATH=$PATH:/home/rpimpi/mpi-install/bin" >> .bashrc
-
+    cd ..
+    echo "PATH=$PATH:/home/rpimpi/mpi-install/bin" >> .bashrc
+else
+   echo "$FILE not downloaded. Installation aborted."
+fi
 sudo reboot
 
 # Test that MPI works
